1c40d0b0f47e59c9465d4e61bdb24ccba6aa3a45
Sandhra Prathap <sandhraprathap@gmail.com>
2021-08-27 23:02:41 +0530

    fixed icon button issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(home: DinoGuess()));
+}
+
+void rand() {
+  Random random = new Random();
+  int randomNumber = random.nextInt(100);
 }
 
 class DinoGuess extends StatefulWidget {
@@ -13,64 +19,90 @@ class _DinoGuessState extends State<DinoGuess> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Text(
-            "DinoGuess",
-            style: TextStyle(
-                color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "DinoGuess",
+          style: TextStyle(
+              color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [
+              0.15,
+              0.5,
+              1
+            ],
+                colors: [
+              Color.fromRGBO(0, 255, 240, 100),
+              Color.fromRGBO(21, 175, 166, 100),
+              Color.fromRGBO(44, 91, 89, 100),
+            ])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Enter your ",
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                    Text(
+                      "Guess",
+                      style: TextStyle(color: Colors.black, fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: TextFormField(
+                  style: TextStyle(fontSize: 95, fontWeight: FontWeight.w300),
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      labelText: ''),
+                ),
+              ),
+              Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 80,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.fitHeight,
+                            image: AssetImage("image/black.png")),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 75,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.fitHeight,
+                            image: AssetImage("image/white.png")),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-        body: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  stops: [
-                0.15,
-                0.5,
-                1
-              ],
-                  colors: [
-                Color.fromRGBO(0, 255, 240, 100),
-                Color.fromRGBO(21, 175, 166, 100),
-                Color.fromRGBO(44, 91, 89, 100),
-              ])),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Enter your ",
-                        style: TextStyle(color: Colors.white, fontSize: 24),
-                      ),
-                      Text(
-                        "Guess",
-                        style: TextStyle(color: Colors.black, fontSize: 24),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(  
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: TextFormField(
-                    style: TextStyle(fontSize: 85,fontWeight: FontWeight.w300),
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                        border: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white)),
-                        labelText: ''),
-                  ),
-                ),    
-                      IconButton(onPressed:(){}, icon:(Image.asset("image/icon6.png")),iconSize:150,),                                    
-                       IconButton(onPressed:(){}, icon:(Image.asset("image/icon5.png")),iconSize:150,)
-                         ],),
-            ),
-          ),
-        );
+      ),
+    );
   }
 }
